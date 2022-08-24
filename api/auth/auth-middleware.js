bf0a7255173929b003f094c5654757e84dc2b1dd
Andrew Clark <andrewclark@Andrews-MacBook-Pro-2.local>
2022-08-23 22:06:05 -0500

    models added, middleware funcs not completed

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,25 @@
+const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const Users = require('../users/users-model')
 
 const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token == null) {
+    next({ status: 401, message: "Token required" })
+    return;
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      console.log(err)
+      next({ status: 401, message: "Token invalid" })
+    } else {
+      req.decodedToken = decodedToken
+      next()
+    }
+  })
+}
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -16,9 +35,15 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-}
 
 const only = role_name => (req, res, next) => {
+  if (req.decodedToken.role_name !== role_name) {
+    console.log('auth-middleware might need to fix this here')
+    // ?????? Pull the decoded token from the req object, to avoid verifying it again! ??????
+    next({ status: 403, message: "This is not for you" })
+  } else {
+    next()
+  }
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -32,7 +57,14 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
+  const users = Users.findBy({ username: req.body.username });
+  if (users.length) {
+    req.user = users[0]
+    next()
+  } else {
+    next({ status: 401, message: "Invalid credentials" })
+  }
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -44,6 +76,7 @@ const checkUsernameExists = (req, res, next) => {
 
 
 const validateRoleName = (req, res, next) => {
+  
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
